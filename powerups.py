1c2ff943827fbfa5850c8f97e41bb46923b08128
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-02-14 20:04:43 +0530

    Randomize Powerups

--- powerups.py
+++ powerups.py
@@ -6,37 +6,37 @@ import random
 obj_powerup = PowerUp(0, 0)
 
 def generate_powerup(x, y):
-	# pno = random.randint(1, 6)
-	# if pno == 1:
-	# 	obj_powerup.add_power("d", obj_board.grid[y][x])
-	# 	obj_powerup.update_position(x, y)
-	# 	obj_powerup.timestamp()
-	# 	obj_powerups.append(obj_powerup)
-	# elif pno == 2:
-	# 	obj_powerup.add_power("e", obj_board.grid[y][x])
-	# 	obj_powerup.update_position(x, y)
-	# 	obj_powerup.timestamp()
-	# 	obj_powerups.append(obj_powerup)
-	# elif pno == 3:
-	# 	obj_powerup.add_power("s", obj_board.grid[y][x])
-	# 	obj_powerup.update_position(x, y)
-	# 	obj_powerup.timestamp()
-	# 	obj_powerups.append(obj_powerup)
-	# elif pno == 4:
-	obj_powerup.add_power("f", obj_board.grid[y][x])
-	obj_powerup.update_position(x, y)
-	obj_powerup.timestamp()
-	obj_powerups.append(obj_powerup)
-	# elif pno == 5:
-	# 	obj_powerup.add_power("t", obj_board.grid[y][x])
-	# 	obj_powerup.update_position(x, y)
-	# 	obj_powerup.timestamp()
-	# 	obj_powerups.append(obj_powerup)
-	# elif pno == 6:
-	# 	obj_powerup.add_power("c", obj_board.grid[y][x])
-	# 	obj_powerup.update_position(x, y)
-	# 	obj_powerup.timestamp()
-	# 	obj_powerups.append(obj_powerup)
+	pno = random.randint(1, 6)
+	if pno == 1:
+		obj_powerup.add_power("d", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
+	elif pno == 2:
+		obj_powerup.add_power("e", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
+	elif pno == 3:
+		obj_powerup.add_power("s", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
+	elif pno == 4:
+		obj_powerup.add_power("f", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
+	elif pno == 5:
+		obj_powerup.add_power("t", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
+	elif pno == 6:
+		obj_powerup.add_power("c", obj_board.grid[y][x])
+		obj_powerup.update_position(x, y)
+		obj_powerup.timestamp()
+		obj_powerups.append(obj_powerup)
 
 def activate_power(power, num_balls = 0):
 	if power == "d":
